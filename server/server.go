fec4296413230e8d1c48c768124140e91744ae5b
dayfine <difancpa@gmail.com>
2018-10-28 10:33:59 -0700

    refine the login form submission

--- server/server.go
+++ server/server.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm() //解析url传递的参数，对于POST则解析响应包的主体（request body）
-	//注意:如果没有调用ParseForm方法，下面无法获取表单的数据
-	fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
+	r.ParseForm()
+
+	fmt.Println(r.Form)
 	fmt.Println("path", r.URL.Path)
 	fmt.Println("scheme", r.URL.Scheme)
 	fmt.Println(r.Form["url_long"])
@@ -19,26 +21,41 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
-	fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
+	fmt.Fprintf(w, "Hello World!")
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //获取请求的方法
+	fmt.Println("method:", r.Method)
+
 	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
 		t, _ := template.ParseFiles("server/login.gtpl")
 		log.Println(t.Execute(w, nil))
 	} else {
-		r.ParseForm()
 		//请求的是登录数据，那么执行登录的逻辑判断
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
+		r.ParseForm()
+		token := r.Form.Get("token")
+		if token != "" {
+			// verify it's a legitmate token
+		} else {
+			// panic
+		}
+
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username")))
+		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+		template.HTMLEscape(w, []byte(r.Form.Get("username")))
 	}
 }
 
 func main() {
-	http.HandleFunc("/", sayhelloName)       //设置访问的路由
-	http.HandleFunc("/login", login)         //设置访问的路由
-	err := http.ListenAndServe(":9090", nil) //设置监听的端口
+	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
+
+	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
